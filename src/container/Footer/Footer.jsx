65793a4f1a4ef1fce9cbe14bc45d4fa52e4b75a4
Raisul999 <raisulislamarnob0@gmail.com>
2022-08-18 15:24:44 +0600

    dynamic year footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -39,7 +39,7 @@ const Footer = () => (
     </div>
 
     <div className="footer__copyright">
-      <p className="p__opensans">2021 Gericht. All Rights reserved.</p>
+      <p className="p__opensans">{new Date().getFullYear()} Gericht. All Rights reserved.</p>
     </div>
 
   </div>
